a9a397addaee4b6b81191eb60d264cd6d5504837
gws <glennws@pm.me>
2025-11-28 13:24:27 +0100

    working solver

--- src/main.rs
+++ src/main.rs
@@ -7,38 +7,89 @@ type Onum = Option<NonZeroU8>;
 
 type State = [[Onum; 9]; 9];
 
-#[derive(Default)]
+#[derive(Default, Copy, Clone, Debug)]
 struct Board(State);
 
+fn unique(data: &[Onum]) -> bool {
+    for n in (1..=9).map(|n| NonZeroU8::new(n)) {
+        if data.iter().filter(|v| **v == n).count() > 1 {
+            return false;
+        }
+    }
+    true
+}
+
 impl Board {
-    fn check_row(&self, i: usize) -> bool {
-        todo!()
+    fn square(&self, row: usize, col: usize) -> [Onum; 9] {
+        let rows = row..(row + 3);
+        let data: Vec<_> = rows
+            .flat_map(|row| {
+                let row = self.0[row];
+                row.into_iter().skip(col).take(3)
+            })
+            .collect();
+        data.try_into().unwrap()
+    }
+    fn check_boxes(&self) -> bool {
+        for row in 0..3 {
+            for col in 0..3 {
+                let data = self.square(row * 3, col * 3);
+                if !unique(&data) {
+                    return false;
+                }
+            }
+        }
+        true
+    }
+
+    fn check_column(&self, column: usize) -> bool {
+        let cols: Box<[_]> = (0..9).map(|row| self.0[row][column]).collect();
+        unique(&cols)
+    }
+    fn check_columns(&self) -> bool {
+        (0..9).all(|i| self.check_column(i))
+    }
+
+    fn check_row(&self, row: usize) -> bool {
+        unique(&self.0[row])
     }
     fn check_rows(&self) -> bool {
         (0..9).all(|i| self.check_row(i))
     }
     fn check(&self) -> bool {
-        todo!()
+        self.check_rows() && self.check_columns() && self.check_boxes()
     }
     fn next_pos(&self) -> Option<usize> {
-        todo!()
+        self.0
+            .iter()
+            .flatten()
+            .enumerate()
+            .filter_map(|(idx, num)| match num {
+                Some(_) => None,
+                None => Some(idx),
+            })
+            .next()
     }
     fn set_pos(&mut self, pos: usize, n: Onum) {
-        todo!()
+        let row = pos / 9;
+        let col = pos % 9;
+        self.0[row][col] = n;
     }
 
     fn solve(mut self) -> Option<Self> {
-        let Some(pos) = self.next_pos() else {
-            if self.check() {
-                return Some(self);
-            }
+        if !self.check() {
             return None;
+        }
+        let Some(pos) = self.next_pos() else {
+            return Some(self);
         };
-        let solved = (0..9).any(|n| {
-            self.set_pos(pos, NonZeroU8::new(n));
-            self.check()
-        });
-        if solved { Some(self) } else { self.solve() }
+        for n in (1..=9).map(|i| NonZeroU8::new(i)) {
+            self.set_pos(pos, n);
+            if let Some(solution) = self.solve() {
+                return Some(solution);
+            }
+        }
+        None
     }
 }
 
@@ -62,5 +113,13 @@ impl Display for Board {
 
 fn main() {
     let board = Board::default();
-    println!("{board}")
+    println!("{board}");
+    let solution = board.solve();
+    match solution {
+        Some(s) => {
+            println!("{s}");
+            println!("{:?}", s.square(0, 0));
+        }
+        None => panic!("no solution"),
+    }
 }
